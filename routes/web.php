d88658baddcd55ac3d0fec1bf1a1da203213f510
Giacomo Sfratato <xbit18@hotmail.it>
2020-05-16 16:39:48 +0200

    solved

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    if(Auth::check()) return redirect()->route('home'); else
     return view('auth/login');
 });
 
