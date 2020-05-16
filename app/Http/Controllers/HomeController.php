e582891388c408cddf67b613b1c98a2f75b6d297
Bryant Sarabia <bryantsarabia@gmail.com>
2020-05-16 15:49:14 +0200

    merge conflict

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,11 +25,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-<<<<<<< HEAD
-        return view('home');
-=======
         $user = Auth::user();
         return view('home', ['user'=>$user]);
->>>>>>> c7183762243e71ebc5194eb3d951a0c897beec83
     }
 }
