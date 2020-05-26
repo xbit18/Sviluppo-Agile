7f4fd3eca5b58152cc4d03db04aa8bcb95acb902
Giacomo Sfratato <xbit18@hotmail.it>
2020-05-26 17:37:34 +0200

    commit

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -39,3 +39,15 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 //     forceTLS: true
 // });
+
+import Echo from "laravel-echo"
+
+window.io = require('socket.io-client');
+
+window.Echo = new Echo({
+    broadcaster: 'socket.io',
+    host: window.location.hostname + ':6001',
+    headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    }
+});
