de914a3e6a4e416e521863f4e943edfcc7ee795b
Bryant Sarabia <bryantsarabia@gmail.com>
2020-06-01 22:02:31 +0200

    fix

--- app/Genre.php
+++ app/Genre.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Genre extends Model
 {
     protected $fillable = ['genre'];
+
+    public function genre(){
+        return $this->belongsToMany('App\Party','party_has_genre');
+     }
+  
 }
