738bb7f5463af7a7c1c3203f70fefc9d98df6843
federix98 <fededm43@gmail.com>
2020-06-02 13:14:16 +0200

    Css fixes

--- resources/views/user/includes/header.blade.php
+++ resources/views/user/includes/header.blade.php
@@ -36,9 +36,7 @@
                         <li><a href="/admin">Admin Panel</a></li>
                     @endif
                     
-                    <li class="{{ Route::currentRouteName() == 'me.parties.show' ? 'current-item' : '' }}" ><a href="{{ route('me.parties.show') }}">My Parties</a></li>
-                    <li class="{{ Route::currentRouteName() == 'party.create' ? 'current-item' : '' }}" ><a href="{{ route('party.create') }}">Create</a></li>
-                    <li class="{{ Route::currentRouteName() == 'parties.index' ? 'current-item' : '' }}" ><a href="{{ route('parties.index') }}">Participate</a></li>
+                   
                     <li><a href="#">{{ Auth::user()->name }}</a>
                       <ul class="dropdown">
                         <li>
@@ -51,11 +49,18 @@
 
                           </li>
                       </ul>
-                    </li>
-                    <li><a href="#"><i class="fa fa-spotify" aria-hidden="true"></i> Spotify</a>
+                    </li> 
+                    <li><a href="#"><i class="fa fa-spotify mr-1" aria-hidden="true"></i> Spotify</a>
                       <ul class="dropdown">
                         <li ><a href="{{ route('spotify.login') }}">Access / Refresh</a></li>
                         <li ><a href="{{ route('spotify.logout') }}">Exit</a></li>
+                      </ul>
+                    </li>
+                    <li><a href="#"><i class="fa fa-users mr-1" aria-hidden="true"></i> Party</a>
+                      <ul class="dropdown">
+                        <li class="{{ Route::currentRouteName() == 'me.parties.show' ? 'current-item' : '' }}" ><a href="{{ route('me.parties.show') }}">My Parties</a></li>
+                        <li class="{{ Route::currentRouteName() == 'party.create' ? 'current-item' : '' }}" ><a href="{{ route('party.create') }}">Create</a></li>
+                        <li class="{{ Route::currentRouteName() == 'parties.index' ? 'current-item' : '' }}" ><a href="{{ route('parties.index') }}">Participate</a></li>
                       </ul>
                     </li>
                     
