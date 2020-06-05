1a1b109b44734cb08a51ae8ab14b4d6cbcd84b03
Xardas7 <taif12340@gmail.com>
2020-06-05 14:38:40 +0200

    ban, kick and vote

--- database/migrations/2020_06_05_113920_user_ban_user.php
+++ database/migrations/2020_06_05_113920_user_ban_user.php
@@ -27,6 +27,8 @@ class UserBanUser extends Migration
      */
     public function down()
     {
-        //
+        Schema::disableForeignKeyConstraints();
+        Schema::dropIfExists('user_participates_party');
+        Schema::enableForeignKeyConstraints();
     }
 }
