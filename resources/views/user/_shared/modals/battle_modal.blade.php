83b9907bb6192e47a1cbbc6711bd3477ab161a34
federix98 <fededm43@gmail.com>
2020-06-08 17:01:38 +0200

    Fix

--- resources/views/user/_shared/modals/battle_modal.blade.php
+++ resources/views/user/_shared/modals/battle_modal.blade.php
@@ -1,22 +1,17 @@
-<div class="modal fade" id="battleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-    aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
+<!-- Modal -->
+<div class="modal fade" id="battleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">Put on the ring</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
+                  <span aria-hidden="true">&times;</span>
                 </button>
+              </div>
+              <div class="modal-body d-flex justify-content-between">
+                <button type="button" id="left_side_button" class="btn poca-btn w-100">Left Side</button>
+                <button type="button" id="right_side_button" class="btn poca-btn w-100">Right Side</button>
+              </div>
             </div>
-            <div class="modal-body">
-                <p>The will be choise by partecipants</p>
-                <button type="button" id="left_side_button" class="btn poca-btn">Left Side</button>
-                <button type="button" id="right_side_button" class="btn poca-btn">Right Side</button>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-
-            </div>
+          </div>
         </div>
-    </div>
-</div>
