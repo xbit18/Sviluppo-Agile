d2b2484add629602053093c2dbf7f73d9ee75437
federix98 <fededm43@gmail.com>
2020-06-08 09:57:52 +0200

    Update player_host_battle.js

--- public/js/player/player_host_battle.js
+++ public/js/player/player_host_battle.js
@@ -50,6 +50,8 @@
 
             var track_uri;
 
+            if(state == null) return;
+
             if (state) {
 
                 // Setting text duration and timeline max value
@@ -421,21 +423,29 @@
 
                         
                     }).catch((error)=> {
+                        var message = '';
                         if (error.response) {
                             // The request was made and the server responded with a status code
                             // that falls out of the range of 2xx
                             console.log(error.response.data);
                             console.log(error.response.status);
                             console.log(error.response.headers);
+                            message += error.response.status + ': ' + error.response.data;
                         } else if (error.request) {
                             // The request was made but no response was received
                             // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
                             // http.ClientRequest in node.js
                             console.log(error.request);
+                            message += error.request;
                         } else {
                             // Something happened in setting up the request that triggered an Error
                             console.log('Error', error.message);
+                            message += error.message;
                         }
+                        Toast.fire({
+                            type: 'error',
+                            title: 'Spotify play error: ' + message
+                            });
                     });
 
                 },
@@ -761,6 +771,10 @@
             })
             .catch(function(error){
                 console.log(error);
+                Toast.fire({
+                    type: 'error',
+                    title: 'Spotify error ( call track details )'
+                    });
             });
 
         }
