7e3d3bf9ca56a1983ff8677e892390cfb38a4820
federix98 <fededm43@gmail.com>
2020-06-11 15:42:22 +0200

    Add e remove song

--- tests/Feature/Party/PartyTest.php
+++ tests/Feature/Party/PartyTest.php
@@ -264,6 +264,29 @@ class PartyTest extends TestCase
         $response->assertStatus(500);
     }
 
+    /** @test */
+    public function add_song() {
+        
+        $song_data = [
+            'track_uri' => 'spotify:track:132qd23f4f'
+        ];
+        $response = $this->actingAs($this->user)->post('/party/'.$this->code.'/tracks/', $song_data);
+
+        $this->assertTrue($this->party->tracks()->where('track_uri', 'spotify:track:132qd23f4f')->count() == 1);
+
+    }
+
+    /** @test */
+    public function remove_song() {
+        
+        $this->actingAs($this->user)->post('/party/'.$this->code.'/tracks/',  ['track_uri' => 'spotify:track:132qd23f4f']);
+        $song_id = $this->party->tracks()->where('track_uri', 'spotify:track:132qd23f4f')->pluck('id')->first();
+
+        $this->actingAs($this->user)->delete('/party/'.$this->code.'/tracks/'.$song_id);
+        $this->assertTrue($this->party->tracks()->where('track_uri', 'spotify:track:132qd23f4f')->count() == 0);
+
+    }
+
     /**
      * Aggiungendo la validazione i campi devono essere così
      * $validatedData = $request->validate([
